2ae5741a754d0f305319ad6476033de5502f1481
Mateus Caruccio <mateus.caruccio@safari.to>
2013-01-10 22:42:56 +0000

    adde transparent broker handler

---
+++ getup/api/broker/index.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import bottle
+from getup import aaa, provider
+from getup.response import response
+
+@aaa.authoritative_user
+@provider.provider
+def get(user, prov, path):
+	url = prov(path)
+	res = url.GET(headers=dict(bottle.request.headers), cookies=bottle.request.cookies)
+	return response(user, res)
+
+@aaa.authoritative_user
+@provider.provider
+def post(user, prov, path):
+	return _data_request(user, prov(path).POST)
+
+@aaa.authoritative_user
+@provider.provider
+def put(user, prov, path):
+	return _data_request(user, prov(path).PUT)
+
+@aaa.authoritative_user
+@provider.provider
+def delete(user, prov, path):
+	return _data_request(user, prov(path).DELETE)
+
+def _data_request(user, method):
+	res = method(data=bottle.request.body.read(), headers=dict(bottle.request.headers), cookies=bottle.request.cookies)
+	return response(user, res)
+
