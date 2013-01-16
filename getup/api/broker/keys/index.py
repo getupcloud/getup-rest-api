d0c3d603c715c2ca21d51c9c3f5d648f9c43c065
Mateus Caruccio <mateus.caruccio@corp.terra.com.br>
2013-01-16 17:31:59 -0200

    continue pubkey devel...

--- getup/api/broker/keys/index.py
+++ getup/api/broker/keys/index.py
@@ -10,12 +10,14 @@ from getup.response import response
 def post(user, prov, api, path):
 	cookies = bottle.request.cookies
 	res = prov.add_key(path=path, body=bottle.request.body.read(), headers=dict(bottle.request.headers), cookies=dict(cookies))
+	print 'BROKER:', res, res.text
 	if res.ok:
 		body = {
 			'title': bottle.request.params.name,
 			'key': '%s %s' % (bottle.request.params.type, bottle.request.params.content),
 		}
 		api_res = api.add_key(body=body, headers=util.filter_headers(), cookies=cookies)
+		print 'GITLAB:', api_res, api_res.text
 		if not api_res.ok:
 			print 'WARNING: Unable to post user key to gitlab:', api_res.status_code
 	return res
