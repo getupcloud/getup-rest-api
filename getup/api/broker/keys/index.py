e45ee5e032518430d8e529fbc9de5aa29bab3dd2
Mateus Caruccio <mateus.caruccio@corp.terra.com.br>
2013-01-17 23:02:29 -0200

    continue pubkey devel...

--- getup/api/broker/keys/index.py
+++ getup/api/broker/keys/index.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import bottle
-from getup import aaa, provider, gitlab, util
+from getup import aaa, provider, gitlab, util, database
 from getup.response import response
 
 @aaa.authoritative_user
