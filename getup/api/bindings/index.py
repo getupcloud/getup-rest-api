420de9763a501372f1f6128a3f0a3efd2d3d85b3
Mateus Caruccio <mateus.caruccio@corp.terra.com.br>
2013-01-21 17:31:17 -0200

    fix

--- getup/api/bindings/index.py
+++ getup/api/bindings/index.py
@@ -14,16 +14,30 @@ def post(user, prov, api, ssh):
 		domain, name, project = params['domain'], params['name'], params['project']
 	except KeyError:
 		return response(user, status=http.HTTP_UNPROCESSABLE_ENTITY)
-	res = prov.get_app(domain=params['domain'], name=params['name'])
-	data = res.json['data']
-	gear = data['git_url']
-	repo = api.get_project(name)
 
-	cmd = "cd /home/git/repositories/%s.git && { git remote add app '%s' || git remote set-url app '%s'; }"  % (project, gear, gear)
+	# retrieve openshift app
+	res = prov.get_app(domain=domain, name=name)
+	if not res.ok:
+		return response(user, res)
+	app = res.json['data']
+
+	# retrieve gitlab project
+	res = api.get_project(name)
+	if not res.ok:
+		return response(user, res)
+	proj = res.json
+
+	# add git remote 'app' to project repository
+	# poiting to openshift gear repository.
+	values = dict(project=project.replace('/', ''), **data)
+	# TODO: parametrize paths and remote name
+	cmd = '''
+	cd /home/git/repositories/%(project)s.git
+	git remote add app '%(git_url)s' || git remote set-url app '%(git_url)s'
+	'''  % values
+
 	ret = ssh.run(cmd)
-	print '>' * 30
-	print ret
-	print '<' * 30
+
 	if ret.returncode != 0:
 		return 'ERROR: %s' % ret.stderr
 	else:
