06b9e3f618e988dfc8bce4f86887b4ce4f6734de
Mateus Caruccio <mateus.caruccio@corp.terra.com.br>
2013-01-21 20:24:10 -0200

    fix

---
+++ getup/api/broker/app/index.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+import bottle
+from getup import aaa, provider
+from getup.response import response
+
+@aaa.authoritative_user
+@provider.provider
+def post(user, prov, path):
+	res = prov(path).POST(data=bottle.request.body.read(), headers=dict(bottle.request.headers), cookies=bottle.request.cookies)
+
+	# register gitlab pubkey into openshift application
+	with open(app.config.webgit['pubkey_file']) as pubkey:
+		key = dict(zip(['type', 'content', 'name'], map(urllib.quote, pubkey.readline().split())))
+	if not prov.broker.rest.user.keys.POST(data=key).ok:
+		print 'error registering gitlab public key'
+
+	return response(user, res)
