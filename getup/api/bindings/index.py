48a18817628a98a01eea0ee2956efb8ec31ceb0f
Mateus Caruccio <mateus@caruccio.com>
2013-02-07 14:40:02 -0200

    missing include

--- getup/api/bindings/index.py
+++ getup/api/bindings/index.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import bottle
-from getup import aaa, provider, codec, gitlab
+from getup import aaa, provider, codec, gitlab, http
 from getup.response import response
 
 @aaa.authoritative_user
