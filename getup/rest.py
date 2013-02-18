9c9b5f1e85db337d70145a3148e610e42d699f07
Mateus Caruccio <mateus@caruccio.com>
2013-02-18 16:02:12 -0300

    fix module name

--- getup/rest.py
+++ getup/rest.py
@@ -3,7 +3,7 @@
 
 import bottle
 import aaa
-import bindings
+import remotes
 
 app = bottle.default_app()
 
@@ -12,33 +12,33 @@ app = bottle.default_app()
 #
 @bottle.get('/getup/rest/projects/<project>/remotes')
 @aaa.user
-def handle_bindings(user, project):
-	'''List all project bindings.
+def handle_remotes(user, project):
+	'''List all project remotes.
 	'''
-	return bindings.list_remotes(user, project)
+	return remotes.list_remotes(user, project)
 
 @bottle.get('/getup/rest/projects/<project>/remotes/<remote>')
 @aaa.user
-def handle_bindings(user, project, remote):
+def handle_remotes(user, project, remote):
 	'''Retrieve project binding.
 	'''
-	return bindings.get_remote(user, project, remote)
+	return remotes.get_remote(user, project, remote)
 
 @bottle.post('/getup/rest/projects/<project>/remotes')
 @aaa.user
-def handle_bindings(user, project):
+def handle_remotes(user, project):
 	'''Bind project to application.
 	'''
 	domain = bottle.request.params.get('domain')
 	application = bottle.request.params.get('application')
-	return bindings.add_remote(user=user, project=project, domain=domain, application=application)
+	return remotes.add_remote(user=user, project=project, domain=domain, application=application)
 
 @bottle.delete('/getup/rest/projects/<project>/remotes/<remote>')
 @aaa.user
-def handle_bindings(user, project, remote):
+def handle_remotes(user, project, remote):
 	'''Delete project binding.
 	'''
-	return bindings.del_remote(user, project, remote)
+	return remotes.del_remote(user, project, remote)
 
 #
 # Broker callbacks
