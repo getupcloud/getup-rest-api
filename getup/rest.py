4c4724320ff7010e9b6abd6473fbc65bcad24a4a
Mateus Caruccio <mateus@caruccio.com>
2013-03-02 00:41:23 -0300

    error reading app name

--- getup/rest.py
+++ getup/rest.py
@@ -139,8 +139,7 @@ def delete_domain(user, domain):
 @response_status_ok
 @aaa.user
 def post_application(user, domain):
-	print '+++', dict(bottle.request.params)
-	res = gitlab.Gitlab().add_project('{name}-{domain}'.format(domain=domain, **bottle.request.params))
+	res = gitlab.Gitlab().add_project('{name}-{domain}'.format(domain=domain, **request_params()))
 	aaa.create_app(user, domain, request_params())
 	return 'OK'
 
